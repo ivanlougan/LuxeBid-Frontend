17a8f7ba38ceac38d0e4b3526545db418e951f86
K Burnett <bluemalek@gmail.com>
2023-03-21 17:52:05 +0000

    UPDATE: authentication link hidden

--- src/utils/games/getGames.js
+++ src/utils/games/getGames.js
@@ -2,7 +2,7 @@
 // Might be best to hide this link in .env?
 export const authentication = async () => {
     try {
-        const response = await fetch("https://id.twitch.tv/oauth2/token?client_id=zzsc5u3kro28y6yizq1zjq27vxwce7&client_secret=m83n6s047uvbe3o77e9wp4iz97m0hg&grant_type=client_credentials", {
+        const response = await fetch(`${process.env.AUTH}`, {
             method: "POST",
             mode: "cors",
             headers: {
