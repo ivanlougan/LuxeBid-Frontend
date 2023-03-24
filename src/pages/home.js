176f31b03fea3e972275549c44c4ae4ad0912220
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-03-24 10:29:07 +0000

    AddingPricesToGames and Checkout page detail

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,43 @@
 import { useState, useEffect } from 'react';
+import { faker } from '@faker-js/faker';
 import "./home.css";
 
 // Components
 
 
-const Home = ({gamesData, basket}) => {
+const Home = ({ gamesData, basket }) => {
     //States?
 
+    const [gameData, setGameData] = useState([]);
+    const [error, setError] = useState(null);
+    const [gameInfo, setGameInfo] = useState([]);
+    const [pricesData, setPriceInfo] = useState([]);
+
     useEffect(() => {
+        const fetchData = async () => {
+
+            try {
+                setError(null);
+
+                const gameInfo = []
+                for (let i = 0; i < 10; i++) {
+
+                    gameInfo.push({
+                        company: faker.company.Name(),
+                        finance: faker.finance.amount(),
+
+                    })
+                }
+                setGameData(gameInfo)
+                console.log(gameData)
+
+            } catch (error) {
+                setError("Information unavailable")
+            }
+        }
+        fetchData();
 
     }, [])
-
 
     return (
         <div className="home-container">
@@ -19,13 +46,14 @@ const Home = ({gamesData, basket}) => {
             <div className="game-cards-container">
                 {gamesData.map((game, index) => {
                     return (
-                    <div className="game-cards" key={index}>
-                        <h3>{game.name}</h3>
-                    </div>
+                        <div className="game-cards" key={index}>
+                            <h3>{game.name}</h3>
+                            <h4>{game.finance}</h4>
+                        </div>
                     )
-            }   )}
+                }
+                )}
             </div>
-
         </div>
     )
 };
