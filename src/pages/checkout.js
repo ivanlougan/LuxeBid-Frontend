ec7593493b0f9d0cf3222a761a2a053c179df9ad
K Burnett <bluemalek@gmail.com>
2023-03-27 10:40:29 +0100

    was having issues on previous working branch

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -12,30 +12,22 @@ const Checkout = ({basket}) => {
     return (
         <div className="checkout-container">
             <h1 id='title'>Your Items</h1>
-           <div className='checkoutBox'>
+            <div className='checkoutBox'>
 
-            <h2>Contact Information</h2>
-            <div className='detailBoxes'><input placeholder='Email'></input>
-            </div>
+                <h2>Contact Information</h2>
+                    <div className='detailBoxes'><input placeholder='Email'></input>
+                </div>
 
-           <div className='ContactBoxes'>
-            <input id='inputBox' placeholder='First name'></input>
-           <input id='inputBox' placeholder='Last name'></input>
-           <input placeholder='Address name'></input>
-           <input placeholder='Post code'></input>
-           
+            <div className='ContactBoxes'>
+                <input id='inputBox' placeholder='First name'></input>
+                <input id='inputBox' placeholder='Last name'></input>
+                <input placeholder='Address name'></input>
+                <input placeholder='Post code'></input>
            </div>
-
-            <h2>Pay below</h2>
-            <h4>Total: £{basket}</h4>
-            <div><button>Pay now</button></div>
-            </div>
-
-
-
-
-
-            
+                <h2>Pay below</h2>
+                <h4>Total: £{basket}</h4>
+                <div><button>Pay now</button></div>
+            </div>          
         </div>
     )
 };
